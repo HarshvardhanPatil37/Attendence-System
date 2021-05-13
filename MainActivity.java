1c16eef00eaf14f045d407a129fbd9e0844ad30c
Harshvardhan Patil <harshvardhanpatil37@gmail.com>
2021-05-13 10:39:47 +0530

    changes 0.1

--- MainActivity.java
+++ MainActivity.java
@@ -16,6 +16,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
+//Main class
 public class MainActivity extends AppCompatActivity {
 
     String Email,Pass;
